aca567464c46dddd70e610fb242100f81e24ff2d
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2025-10-22 16:21:04 +0530

    Interfaces vs. Abstract Classes

--- pages/Interfaces.php
+++ pages/Interfaces.php
@@ -7,9 +7,28 @@ class Cat implements Animal{
     public function makeSound(){
         echo "Meow";
     }
+}
+class Dog implements Animal {
+  public function makeSound() {
+    echo " Bark ";
+  }
+}
+
+class Mouse implements Animal {
+  public function makeSound() {
+    echo " Squeak ";
+  }
    
 }
-$animal = new Cat();
-$animal->makeSound();
- 
+// Create a list of animals
+$cat = new Cat();
+$dog = new Dog();
+$mouse = new Mouse();
+$animals = array($cat, $dog, $mouse);
+
+// Iterate through the list and make each animal sound
+foreach ($animals as $animal) {
+    $animal->makeSound();
+}
+
 ?>
