4f7fefed88527a27faa93a319479a3226527be29
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2025-10-22 14:50:35 +0530

    Construct Function

--- pages/Fruit.php
+++ pages/Fruit.php
@@ -5,15 +5,38 @@ class Fruit{
     public $name;
     public $color;
 
+    // The __construct Function
+    function __construct($name = null, $color = null){
+        $this->name = $name;
+        $this->color = $color;
+        
+    
+    }
+
+    // The __destruct Function
+
+    
+
     // methods
 
-    function setname($name){
+    function setname($name, $color = null){
         $this->name = $name;
+        if ($color !== null) {
+            $this->color = $color;
+        }
     }
 
     function get_name(){    
         return $this->name;
     }    
+    function set_color($color){
+        $this->color = $color;
+    }
+
+    function get_color(){
+        return $this->color;
+    }
+    
 }
 
 // define objects
@@ -21,12 +44,17 @@ class Fruit{
 $apple = new Fruit();
 $banana = new Fruit();
 
-$apple->setname('Apple');
-$banana->setname('Banana');
+$apple->setname('Apple', 'Red');
+$banana->setname('Banana', 'Yellow');
 
 echo $apple->get_name();
 echo "<br>";
 echo $banana->get_name();
+
+echo "<br>";
+
+echo $banana->get_color();
+echo $apple->get_color();
 
 var_dump($apple instanceof Fruit); // true
 ?>
